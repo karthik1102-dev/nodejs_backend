9110a7a0d29d7f38d152b4634c68064a0bf54bde
Arivanantham1003 <119924078+Arivanantham1003@users.noreply.github.com>
2023-05-20 12:04:33 +0530

    changed db & seed roles

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-require("./config/database").connect();
+require("./config/database");
+// require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -13,62 +14,61 @@ const User = require("./model/user");
 
 // Register
 app.post("/register", async (req, res) => {
+  // Our register logic starts here
+  try {
+    // Get user input
+    console.log("req.body");
 
-    // Our register logic starts here
-    try {
-        // Get user input
-        console.log("req.body");
+    console.log(req.body);
 
-        console.log(req.body);
+    const { first_name, last_name, email, password } = req.body;
 
-        const { first_name, last_name, email, password } = req.body;
-
-        // Validate user input
-        if (!(email && password && first_name && last_name)) {
-            res.status(400).send("All input is required");
-        }
-
-        // check if user already exist
-        // Validate if user exist in our database
-        const oldUser = await User.findOne({ email });
-
-        if (oldUser) {
-            return res.status(409).send("User Already Exist. Please Login");
-        }
-
-        //Encrypt user password
-        encryptedPassword = await bcrypt.hash(password, 10);
-
-        // Create user in our database
-        const user = await User.create({
-            first_name,
-            last_name,
-            email: email.toLowerCase(), // sanitize: convert email to lowercase
-            password: encryptedPassword,
-        });
-
-        // Create token
-        const token = jwt.sign(
-            { user_id: user._id, email },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: "2h",
-            }
-        );
-        // save user token
-        user.token = token;
-
-        // return new user
-        res.status(201).json(user);
-    } catch (err) {
-        console.log(err);
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
     }
-    // Our register logic ends here
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
 });
 
 // Login
 app.post("/login", (req, res) => {
-    // our login logic goes here
+  // our login logic goes here
 });
 
 module.exports = app;
--- config/database.js
+++ config/database.js
@@ -1,19 +1,69 @@
+const Role = require("../model/role");
+
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-const { MONGO_URI } = process.env;
+// Define the database URL to connect to.
+const mongoDB =
+  "mongodb+srv://agrolen:Agro2023@agrolen.ami6yvq.mongodb.net/agrolen?retryWrites=true&w=majority";
 
-exports.connect = () => {
-  // Connecting to the database
-    mongoose.connect("mongodb://localhost:27017/maapay_backend_v1", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-      console.log("Successfully connected to database");
-    })
-    .catch((error) => {
-      console.log("database connection failed. exiting now...");
-      console.error(error);
-      process.exit(1);
-    });
-};
+// Wait for database to connect, logging an error if there is a problem
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+
+const seedRoles = [
+  {
+    name: "admin",
+    status: 1,
+  },
+  {
+    name: "provider",
+    status: 1,
+  },
+  {
+    name: "organizer",
+    status: 1,
+  },
+  {
+    name: "supervisior",
+    status: 1,
+  },
+  {
+    name: "operator",
+    status: 1,
+  },
+  {
+    name: "farmer",
+    status: 1,
+  },
+  {
+    name: "customer",
+    status: 1,
+  },
+];
+
+db.on("connected", () => {
+  console.log("DB Connected");
+  const seedDB = async () => {
+    const dataCount = await Role.count();
+    if (dataCount === 0) {
+      try {
+        await Role.insertMany(seedRoles);
+        console.log("documents successfully inserted");
+      } catch (err) {
+        console.error(
+          `Something went wrong trying to insert the new documents: ${err}\n`
+        );
+      }
+    }
+  };
+  seedDB().then(() => {
+    mongoose.connection.close();
+  });
+});
+
+db.on("error", (err) => {
+  console.log("Error Connecting Db", err);
+});
